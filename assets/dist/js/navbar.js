fcb2b2af4d567aa215976365fd781b5564f0be9a
guantou <mole30608@gmail.com>
2022-04-13 00:30:25 +0800

    good

---
+++ assets/dist/js/navbar.js
@@ -0,0 +1,23 @@
+navbar='\
+    <nav class="navbar navbar-expand-sm navbar-dark" aria-label="Third navbar example">\
+      <div class="container-md">\
+        <a class="navbar-brand" href="/index.html">就只是一個罐頭</a>\
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">\
+          <span class="navbar-toggler-icon"></span></button>\
+        <div class="collapse navbar-collapse " id="navbarsExample03">\
+          <ul class="navbar-nav me-auto mb-2 mb-sm-0">\
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="/index.html">首頁</a></li>\
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="/CTF/index.html">CTF文章</a></li>\
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="/TECH/index.html">技術文章</a></li>\
+            <li class="nav-item dropdown">\
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Side Project</a>\
+              <ul class="dropdown-menu" aria-labelledby="dropdown03">\
+                <li><a class="dropdown-item" href="/STG/index.html">課表產生器</a></li>\
+              </ul>\
+            </li>\
+          </ul>\
+        </div>\
+      </div>\
+    </nav>\
+    ';
+document.write(navbar);
