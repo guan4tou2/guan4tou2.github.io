c59adef110017646283ed928eacf2c705e93a75b
guan4tou2 <mole30608@gmail.com>
2025-06-04 00:21:06 +0800

    add gothub action

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://guan4tou2.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
